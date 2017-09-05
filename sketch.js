620aac735d1d390350e06502fdb526cc619eee29
Indigo Lily <yulex.42@gmail.com>
2017-09-05 09:23:00 -0300

    Got circle packing working

--- sketch.js
+++ sketch.js
@@ -47,19 +47,45 @@ function drawQuadrants(x, y, r = 20, a = 0) {
 
 let circles = [];
 
+Array.prototype.min = function() {
+  this.reduce( (min,val) => val < min ? val : min, Infinity );
+}
+
+function dist(x1, y1, x2, y2) {
+  return Math.hypot(x1-x2, y1-y2);
+}
+
 function draw() {
-  for(let i = 0; i < 1; i++) {
+  let n = circles.length**.5 + 1;
+  for(let i = 0; i < n; i++) {
     let x = Math.random() * width,
         y = Math.random() * height,
-        a = Math.random() * TAU,
-        r = Math.hypot(width, height);
+        a = Math.random() * TAU;
 
+    let min = Infinity; // has to be declared outside of loop so I can use it later
     for(let j = 0; j < circles.length; j++) {
-      let it = circles[i];
+      let it = circles[j];
+      let d = dist(it.x, it.y, x, y);
+      if (it.r > d) {
+        j = -1;
+        min = Infinity;
+        x = Math.random() * width;
+        y = Math.random() * height;
+        continue;
+      }
+      if (d - it.r < min) min = d - it.r;
     }
+
+    if (x < min) min = x;
+    if (width - x < min) min = width - x;
+    if (y < min) min = y;
+    if (height - y < min) min = height - y;
+
+    r = min * .5;
+
     drawQuadrants(x, y, r, a);
     circles.push({x: x, y: y, r: r});
   }
-  setTimeout(draw, 2000);
+  requestAnimationFrame(draw);
 }
 draw();
