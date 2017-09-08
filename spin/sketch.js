81331b4c957d015bbbe4fe87d04c9c5f0b33eb0b
Indigo Lily <yulex.42@gmail.com>
2017-09-07 22:44:53 -0300

    Added spinning version

---
+++ spin/sketch.js
@@ -0,0 +1,175 @@
+const canvas = document.getElementById('cnv');
+const c = canvas.getContext('2d');
+
+let width  = canvas.width  = window.innerWidth;
+let height = canvas.height = window.innerHeight;
+let sep = .5;
+let speed = 1;
+let shadow = true;
+
+const PI = Math.PI;
+const TAU = PI * 2;
+
+c.shadowColor = 'rgba(0, 0, 0, 0.5)';
+
+var COLOURS = [
+  '#e34',
+  '#06f',
+  '#fff',
+  '#111',
+];
+let circles = [];
+
+function randColour() {
+  let r = (Math.random()*16|0).toString(16),
+      g = (Math.random()*16|0).toString(16),
+      b = (Math.random()*16|0).toString(16);
+  if (Math.random() > .2) return '#'+r+g+b;
+  else return '#'+r+r+r;
+}
+
+/*window.addEventListener('click', () => {
+  width  = canvas.width  = window.innerWidth;
+  height = canvas.height = window.innerHeight;
+  sep = 0.4 + Math.random() * 0.7;
+  c.shadowColor = shadow ? 'rgba(0, 0, 0, 0.5)' : 'rgba(0, 0, 0, 0)';
+  circles = [];
+  COLOURS = [
+    randColour(),
+    randColour(),
+    randColour(),
+    randColour(),
+  ];
+});*/
+
+function resize() {
+  c.clearRect(0,0,width,height);
+  width  = canvas.width  = window.innerWidth;
+  height = canvas.height = window.innerHeight;
+  circles = [];
+}
+
+window.addEventListener('keydown', e => {
+  switch(e.keyCode) {
+    case 38: // up
+      speed++;
+      break;
+
+    case 40: // down
+      if (speed > 1) speed--;
+      break;
+
+    case 82: // r(esize)
+      resize();
+      break;
+
+    case 83: // s(hadow)
+      c.clearRect(0,0,width,height);
+
+      shadow = !shadow;
+
+      circles.forEach(x => {
+        drawQuadrants(x.x, x.y, x.r, x.a, x.c);
+      });
+      break;
+
+    case 80: // p (separation)
+      resize();
+      sep = 0.5 + Math.random();
+      break;
+
+    case 67: // c(olour)
+      c.clearRect(0,0,width,height);
+
+      COLOURS = [
+        randColour(),
+        randColour(),
+        randColour(),
+        randColour(),
+      ];
+
+      circles.forEach(x => {
+        let c = shuffle(COLOURS);
+        x.c = c;
+        drawQuadrants(x.x, x.y, x.r, x.a, x.c);
+      });
+      break;
+
+    default:
+      console.log(e.keyCode);
+  }
+});
+
+function shuffle(array) {
+  let copy = [...array], out = [];
+  while(copy.length) {
+    let choice = Math.floor(Math.random() * copy.length);
+    out.push(copy.splice(choice, 1)[0]);
+  }
+  return out;
+}
+
+function drawQuadrants(x, y, r, a, colours) {
+  c.save();
+  c.translate(x, y);
+  c.rotate(a);
+  for(let i = 0; i < 4; i++) {
+    c.strokeStyle = c.fillStyle = colours[i];
+    c.shadowBlur = r**.5;
+    c.shadowColor = shadow ? 'rgba(0, 0, 0, 0.5)' : 'rgba(0, 0, 0, 0)';
+    let a = TAU/4 * i;
+
+    c.beginPath();
+    c.arc(0, 0, r, a, a + TAU/4);
+    c.lineTo(0, 0);
+    c.fill();
+    c.shadowBlur = 0;
+    c.stroke();
+  }
+  c.restore();
+}
+
+function dist(x1, y1, x2, y2) {
+  return Math.hypot(x1-x2, y1-y2);
+}
+
+function draw() {
+  for(let i = 0; i < speed; i++) {
+    let x = Math.random() * width,
+        y = Math.random() * height,
+        a = Math.random() * TAU,
+        c = shuffle(COLOURS);
+
+    let min = Infinity;
+    for(let j = 0; j < circles.length; j++) {
+      let it = circles[j];
+      let d = dist(it.x, it.y, x, y);
+      if (it.r > d) {
+        j = -1;
+        min = Infinity;
+        x = Math.random() * width;
+        y = Math.random() * height;
+        continue;
+      }
+      if (d - it.r < min) min = d - it.r;
+    }
+
+    if (x < min) min = x;
+    if (width - x < min) min = width - x;
+    if (y < min) min = y;
+    if (height - y < min) min = height - y;
+
+    r = min * sep;
+
+    circles.push({x: x, y: y, r: r, a: a, c: c});
+    circles.sort((a,b) => b.r - a.r);
+  }
+  c.clearRect(0,0,width,height);
+  circles.forEach(x => {
+    x.a += PI/1000 * (Math.hypot(width,height)/x.r);
+    drawQuadrants(x.x, x.y, x.r, x.a, x.c);
+  });
+  requestAnimationFrame(draw);
+}
+
+draw();
