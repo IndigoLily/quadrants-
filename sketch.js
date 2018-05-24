0b384e11f93f8ab38afa329af18c31ce451ae43e
Lily <yulex.42@gmail.com>
2018-05-24 20:47:48 -0300

    Switched to dat.GUI

--- sketch.js
+++ sketch.js
@@ -1,24 +1,70 @@
 const canvas = document.getElementById('cnv');
 const c = canvas.getContext('2d');
 
-let width  = canvas.width  = window.innerWidth;
-let height = canvas.height = window.innerHeight;
-let sep = .5;
-let speed = 1;
-let shadow = true;
+var width  = canvas.width  = window.innerWidth;
+var height = canvas.height = window.innerHeight;
+
+window.addEventListener('resize', resize);
+
+var should_redraw = false;
+
+var circles = [];
+
+
+
+const gui = new dat.GUI();
+const opt = {};
+
+opt['Restart'] = resize;
+gui.add(opt, 'Restart');
+
+opt['Multiplier'] = 0.5;
+const sep = gui.add(opt, 'Multiplier').min(0).max(2);
+sep.onChange(resize);
+
+opt['Speed'] = 1;
+gui.add(opt, 'Speed').min(0).step(1);
+
+opt['Shadow'] = true;
+const shadow = gui.add(opt, 'Shadow');
+shadow.onChange(() => should_redraw = true);
+
+opt['Random colours'] = function() {
+  clr1.setValue(randColour());
+  clr2.setValue(randColour());
+  clr3.setValue(randColour());
+  clr4.setValue(randColour());
+};
+gui.add(opt, 'Random colours');
+
+const clr = gui.addFolder('Colours');
+
+var COLOURS = ['#ef3340', '#06f', '#fff', '#111'];
+opt['Colour 1'] = COLOURS[0];
+opt['Colour 2'] = COLOURS[1];
+opt['Colour 3'] = COLOURS[2];
+opt['Colour 4'] = COLOURS[3];
+
+const clr1 = clr.addColor(opt, 'Colour 1');
+const clr2 = clr.addColor(opt, 'Colour 2');
+const clr3 = clr.addColor(opt, 'Colour 3');
+const clr4 = clr.addColor(opt, 'Colour 4');
+clr1.onChange(clr => {COLOURS[0] = clr; should_redraw = true;});
+clr2.onChange(clr => {COLOURS[1] = clr; should_redraw = true;});
+clr3.onChange(clr => {COLOURS[2] = clr; should_redraw = true;});
+clr4.onChange(clr => {COLOURS[3] = clr; should_redraw = true;});
+
+
 
 const PI = Math.PI;
 const TAU = PI * 2;
 
 c.shadowColor = 'rgba(0, 0, 0, 0.5)';
 
-var COLOURS = [
-  '#e34',
-  '#06f',
-  '#fff',
-  '#111',
-];
-let circles = [];
+function redraw() {
+    c.clearRect(0, 0, width, height);
+    circles.forEach(circle => drawQuadrants(circle));
+}
 
 function randColour() {
   let r = (Math.random()*16|0).toString(16),
@@ -28,77 +74,11 @@ function randColour() {
   else return '#'+r+r+r;
 }
 
-/*window.addEventListener('click', () => {
-  width  = canvas.width  = window.innerWidth;
-  height = canvas.height = window.innerHeight;
-  sep = 0.4 + Math.random() * 0.7;
-  c.shadowColor = shadow ? 'rgba(0, 0, 0, 0.5)' : 'rgba(0, 0, 0, 0)';
-  circles = [];
-  COLOURS = [
-    randColour(),
-    randColour(),
-    randColour(),
-    randColour(),
-  ];
-});*/
-
 function resize() {
-  c.clearRect(0,0,width,height);
   width  = canvas.width  = window.innerWidth;
   height = canvas.height = window.innerHeight;
   circles = [];
 }
-
-window.addEventListener('keydown', e => {
-  switch(e.keyCode) {
-    case 38: // up
-      speed++;
-      break;
-
-    case 40: // down
-      if (speed > 1) speed--;
-      break;
-
-    case 82: // r(esize)
-      resize();
-      break;
-
-    case 83: // s(hadow)
-      c.clearRect(0,0,width,height);
-
-      shadow = !shadow;
-
-      circles.forEach(x => {
-        drawQuadrants(x.x, x.y, x.r, x.a, x.c);
-      });
-      break;
-
-    case 80: // p (separation)
-      resize();
-      sep = 0.5 + Math.random();
-      break;
-
-    case 67: // c(olour)
-      c.clearRect(0,0,width,height);
-
-      COLOURS = [
-        randColour(),
-        randColour(),
-        randColour(),
-        randColour(),
-      ];
-
-      circles.forEach(x => {
-        let c = shuffle(COLOURS);
-        x.c = c;
-        drawQuadrants(x.x, x.y, x.r, x.a, x.c);
-      });
-      break;
-
-    default:
-      console.log(e.keyCode);
-  }
-});
 
 function shuffle(array) {
   let copy = [...array], out = [];
@@ -109,18 +89,18 @@ function shuffle(array) {
   return out;
 }
 
-function drawQuadrants(x, y, r, a, colours) {
+function drawQuadrants(circle) {
   c.save();
-  c.translate(x, y);
-  c.rotate(a);
+  c.translate(circle.x, circle.y);
+  c.rotate(circle.a);
   for(let i = 0; i < 4; i++) {
-    c.strokeStyle = c.fillStyle = colours[i];
-    c.shadowBlur = r**.5;
-    c.shadowColor = shadow ? 'rgba(0, 0, 0, 0.5)' : 'rgba(0, 0, 0, 0)';
+    c.strokeStyle = c.fillStyle = COLOURS[circle.c[i]];
+    c.shadowBlur = circle.r**0.5;
+    c.shadowColor = opt['Shadow'] ? 'rgba(0, 0, 0, 0.5)' : 'rgba(0, 0, 0, 0)';
     let a = TAU/4 * i;
 
     c.beginPath();
-    c.arc(0, 0, r, a, a + TAU/4);
+    c.arc(0, 0, Math.max(circle.r - c.lineWidth / 2, 0), a, a + TAU/4);
     c.lineTo(0, 0);
     c.fill();
     c.shadowBlur = 0;
@@ -134,11 +114,16 @@ function dist(x1, y1, x2, y2) {
 }
 
 function draw() {
-  for(let i = 0; i < speed; i++) {
+  if (should_redraw) {
+      redraw();
+      should_redraw = false;
+  }
+
+  for(let i = 0; i < opt['Speed']; i++) {
     let x = Math.random() * width,
         y = Math.random() * height,
         a = Math.random() * TAU,
-        c = shuffle(COLOURS);
+        c = shuffle([0, 1, 2, 3]);
 
     let min = Infinity;
     for(let j = 0; j < circles.length; j++) {
@@ -159,12 +144,13 @@ function draw() {
     if (y < min) min = y;
     if (height - y < min) min = height - y;
 
-    r = min * sep;
+    r = min * opt['Multiplier'];
 
-    drawQuadrants(x, y, r, a, c);
-    circles.push({x: x, y: y, r: r, a: a, c: c});
-    circles.sort((a,b) => b.r - a.r);
+    let circle = {x, y, r, a, c};
+    drawQuadrants(circle);
+    circles.push(circle);
   }
+
   requestAnimationFrame(draw);
 }
 
